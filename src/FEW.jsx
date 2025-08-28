204aa4c692d9f8553ae1a13194894260fe540815
Godwill993 <lendzelegoodwill@gmail.com>
2025-08-28 14:10:56 +0100

    icons added

---
+++ src/FEW.jsx
@@ -0,0 +1,16 @@
+function Qualification (){
+    return(
+        <>
+         <div className="left-sec">
+            <h3>My Skills</h3>
+            
+            <p>Website development</p>
+            <p>App development</p>
+            <p>Cloud management</p>
+         </div>
+
+        </>
+    );
+}
+
+export default Qualification;
